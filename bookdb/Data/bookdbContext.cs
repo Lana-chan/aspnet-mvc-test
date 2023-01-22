2b9f5adda910247e2cf40f0f63f77d11935a2275
Erin Pinheiro <erin.manal@gmail.com>
2023-01-22 18:55:32 +0000

    initial seed data for books and authors

--- bookdb/Data/bookdbContext.cs
+++ bookdb/Data/bookdbContext.cs
@@ -7,15 +7,30 @@ using bookdb.Models;
 
 namespace bookdb.Data
 {
-    public class bookdbContext : DbContext
-    {
-        public bookdbContext (DbContextOptions<bookdbContext> options)
-            : base(options)
-        {
-        }
+	public class bookdbContext : DbContext
+	{
+		public bookdbContext (DbContextOptions<bookdbContext> options)
+			: base(options)
+		{
+		}
 
-        public DbSet<bookdb.Models.Book> Book { get; set; } = default!;
+		public DbSet<bookdb.Models.Book> Book { get; set; } = default!;
 
-        public DbSet<bookdb.Models.BookAuthor> BookAuthor { get; set; } = default!;
-    }
+		public DbSet<bookdb.Models.BookAuthor> BookAuthor { get; set; } = default!;
+
+		// get an author or create new one by passing author name
+		public BookAuthor FindOrCreateAuthor(string AuthorName)
+		{
+			var author = BookAuthor.FirstOrDefault(i => i.Name == AuthorName);
+
+			if (author == null)
+			{
+				author = new BookAuthor{Name = AuthorName};
+				BookAuthor.Add(author);
+				SaveChanges();
+			}
+
+			return author;
+		}
+	}
 }
---
+++ bookdb/Models/SeedData.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using bookdb.Data;
+using System;
+using System.Linq;
+
+namespace bookdb.Models;
+
+public static class SeedData
+{
+	public static void Initialize(IServiceProvider serviceProvider)
+	{
+		using (var context = new bookdbContext(
+			serviceProvider.GetRequiredService<
+				DbContextOptions<bookdbContext>>()))
+		{
+			// Populate authors if not found
+			if (!context.BookAuthor.Any())
+			{
+				context.BookAuthor.AddRange(
+					new BookAuthor
+					{
+						Name = "Terry Pratchett"
+					},
+					new BookAuthor
+					{
+						Name = "Douglas Adams"
+					},
+					new BookAuthor
+					{
+						Name = "Douglas Coupland"
+					}
+				);
+				context.SaveChanges();
+			}
+
+			// Populate books if not found
+			if (!context.Book.Any())
+			{
+				context.Book.AddRange(
+					new Book
+					{
+						Title = "Mort",
+						ReleaseDate = DateTime.Parse("1987-11-12"),
+						AuthorId = context.FindOrCreateAuthor("Terry Pratchett").Id
+					}
+				);
+				context.SaveChanges();
+			}
+		}
+	}
+}
